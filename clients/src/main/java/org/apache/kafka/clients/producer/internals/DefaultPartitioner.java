688c4b0ab97272f75a703ee88cb865bf8efe2b78
lee9210 <597969610@qq.com>
2020-10-16 23:16:31 +0800

    DefaultPartitioner 注释

--- clients/src/main/java/org/apache/kafka/clients/producer/internals/DefaultPartitioner.java
+++ clients/src/main/java/org/apache/kafka/clients/producer/internals/DefaultPartitioner.java
@@ -23,6 +23,10 @@ import org.apache.kafka.common.utils.Utils;
 import java.util.Map;
 
 /**
+ * 默认分区策略
+ * 1. 如果record中指定了分区，则使用它
+ * 2. 如果没有指定分区，但存在一个key，则根据key的hash选择一个分区
+ * 3. 如果没有分区或键，则根据batch是否满选择
  * The default partitioning strategy:
  * <ul>
  * <li>If a partition is specified in the record, use it
@@ -33,11 +37,14 @@ import java.util.Map;
  */
 public class DefaultPartitioner implements Partitioner {
 
+    /** 黏性分区缓存 */
     private final StickyPartitionCache stickyPartitionCache = new StickyPartitionCache();
 
+    @Override
     public void configure(Map<String, ?> configs) {}
 
     /**
+     * 根据record获取分区
      * Compute the partition for the given record.
      *
      * @param topic The topic name
@@ -47,11 +54,13 @@ public class DefaultPartitioner implements Partitioner {
      * @param valueBytes serialized value to partition on or null
      * @param cluster The current cluster metadata
      */
+    @Override
     public int partition(String topic, Object key, byte[] keyBytes, Object value, byte[] valueBytes, Cluster cluster) {
         return partition(topic, key, keyBytes, value, valueBytes, cluster, cluster.partitionsForTopic(topic).size());
     }
 
     /**
+     * 根据record计算
      * Compute the partition for the given record.
      *
      * @param topic The topic name
@@ -64,6 +73,7 @@ public class DefaultPartitioner implements Partitioner {
      */
     public int partition(String topic, Object key, byte[] keyBytes, Object value, byte[] valueBytes, Cluster cluster,
                          int numPartitions) {
+        // 如果key为空，则根据黏性分区策略分配
         if (keyBytes == null) {
             return stickyPartitionCache.partition(topic, cluster);
         }
@@ -71,12 +81,17 @@ public class DefaultPartitioner implements Partitioner {
         return Utils.toPositive(Utils.murmur2(keyBytes)) % numPartitions;
     }
 
+    @Override
     public void close() {}
     
     /**
+     * 如果当前粘分区的批处理完成，则更改粘分区。
+     * 或者，如果没有确定粘分区，则设置一个。
+     *
      * If a batch completed for the current sticky partition, change the sticky partition. 
      * Alternately, if no sticky partition has been determined, set one.
      */
+    @Override
     public void onNewBatch(String topic, Cluster cluster, int prevPartition) {
         stickyPartitionCache.nextPartition(topic, cluster, prevPartition);
     }
