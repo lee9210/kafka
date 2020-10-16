9d6d218bf55736f4b0c3356a834196527495b545
lee9210 <597969610@qq.com>
2020-10-16 22:40:59 +0800

    BufferSupplier 注释

--- clients/src/main/java/org/apache/kafka/common/record/BufferSupplier.java
+++ clients/src/main/java/org/apache/kafka/common/record/BufferSupplier.java
@@ -24,6 +24,11 @@ import java.util.HashMap;
 import java.util.Map;
 
 /**
+ * 用于缓存字节缓冲区的简单非线程安全接口。
+ * 这适用于一些简单的情况，
+ * 比如确保给定的KafkaConsumer在迭代获取的记录时重用相同的解压缩缓冲区。
+ * 对于小的记录批，分配一个潜在的大缓冲区(LZ4为64 KB)将控制batch中record的解压缩和迭代的成本。
+ *
  * Simple non-threadsafe interface for caching byte buffers. This is suitable for simple cases like ensuring that
  * a given KafkaConsumer reuses the same decompression buffer when iterating over fetched records. For small record
  * batches, allocating a potentially large buffer (64 KB for LZ4) will dominate the cost of decompressing and
@@ -49,20 +54,30 @@ public abstract class BufferSupplier implements AutoCloseable {
     }
 
     /**
+     * 提供一个具有所需容量的缓冲区。这可能会返回一个缓存缓冲区或分配一个新实例。
+     *
      * Supply a buffer with the required capacity. This may return a cached buffer or allocate a new instance.
      */
     public abstract ByteBuffer get(int capacity);
 
     /**
+     * 返回所提供的缓冲区，以便后续调用' get '时重用。
+     *
      * Return the provided buffer to be reused by a subsequent call to `get`.
      */
     public abstract void release(ByteBuffer buffer);
 
     /**
+     * 释放与supplier相关的所有资源。
+     *
      * Release all resources associated with this supplier.
      */
+    @Override
     public abstract void close();
 
+    /**
+     * BufferSupplier的默认实现
+     */
     private static class DefaultSupplier extends BufferSupplier {
         // We currently use a single block size, so optimise for that case
         private final Map<Integer, Deque<ByteBuffer>> bufferMap = new HashMap<>(1);
@@ -70,10 +85,11 @@ public abstract class BufferSupplier implements AutoCloseable {
         @Override
         public ByteBuffer get(int size) {
             Deque<ByteBuffer> bufferQueue = bufferMap.get(size);
-            if (bufferQueue == null || bufferQueue.isEmpty())
+            if (bufferQueue == null || bufferQueue.isEmpty()) {
                 return ByteBuffer.allocate(size);
-            else
+            } else {
                 return bufferQueue.pollFirst();
+            }
         }
 
         @Override
@@ -95,6 +111,8 @@ public abstract class BufferSupplier implements AutoCloseable {
     }
 
     /**
+     * 简单的缓冲区供应单线程使用。它缓存一个单一的缓冲区，该缓冲区根据需要单调增长，以满足分配请求。
+     *
      * Simple buffer supplier for single-threaded usage. It caches a single buffer, which grows
      * monotonically as needed to fulfill the allocation request.
      */
