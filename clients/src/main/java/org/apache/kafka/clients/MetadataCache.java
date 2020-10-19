34409165bec6aac7cf2cdf1476a75fc7adb88098
lee9210 <597969610@qq.com>
2020-10-19 20:50:21 +0800

    MetadataCache 注释

--- clients/src/main/java/org/apache/kafka/clients/MetadataCache.java
+++ clients/src/main/java/org/apache/kafka/clients/MetadataCache.java
@@ -38,16 +38,25 @@ import java.util.function.Predicate;
 import java.util.stream.Collectors;
 
 /**
+ * Kafka集群中节点、主题和分区的内部可变缓存。
+ * 这将保持一个最新的集群实例，该实例为读访问进行了优化。
+ *
  * An internal mutable cache of nodes, topics, and partitions in the Kafka cluster. This keeps an up-to-date Cluster
  * instance which is optimized for read access.
  */
 public class MetadataCache {
     private final String clusterId;
+    /** 节点 */
     private final Map<Integer, Node> nodes;
+    /** 未授权的主题 */
     private final Set<String> unauthorizedTopics;
+    /** 无效的主题 */
     private final Set<String> invalidTopics;
+    /** 内部主题 */
     private final Set<String> internalTopics;
+    /** controller所在node */
     private final Node controller;
+    /** partition元数据 */
     private final Map<TopicPartition, PartitionMetadata> metadataByPartition;
 
     private Cluster clusterInstance;
@@ -89,10 +98,16 @@ public class MetadataCache {
         }
     }
 
+    /**
+     * 获取partition元数据缓存
+     */
     Optional<PartitionMetadata> partitionMetadata(TopicPartition topicPartition) {
         return Optional.ofNullable(metadataByPartition.get(topicPartition));
     }
 
+    /**
+     * 获取node
+     */
     Optional<Node> nodeById(int id) {
         return Optional.ofNullable(nodes.get(id));
     }
@@ -110,6 +125,9 @@ public class MetadataCache {
     }
 
     /**
+     * 将元数据缓存的内容与所提供的元数据合并，返回新的元数据缓存。
+     * 所提供的元数据被假定比缓存的元数据更近，因此所有重复的元数据都将被覆盖。
+     *
      * Merges the metadata cache's contents with the provided metadata, returning a new metadata cache. The provided
      * metadata is presumed to be more recent than the cache's metadata, and therefore all overlapping metadata will
      * be overridden.
@@ -131,28 +149,32 @@ public class MetadataCache {
                             Set<String> addInternalTopics,
                             Node newController,
                             BiPredicate<String, Boolean> retainTopic) {
-
+        // 应该保留的主题
         Predicate<String> shouldRetainTopic = topic -> retainTopic.test(topic, internalTopics.contains(topic));
-
+        // 新的partition元数据
         Map<TopicPartition, PartitionMetadata> newMetadataByPartition = new HashMap<>(addPartitions.size());
         for (PartitionMetadata partition : addPartitions) {
             newMetadataByPartition.put(partition.topicPartition, partition);
         }
+        // 覆盖
         for (Map.Entry<TopicPartition, PartitionMetadata> entry : metadataByPartition.entrySet()) {
             if (shouldRetainTopic.test(entry.getKey().topic())) {
                 newMetadataByPartition.putIfAbsent(entry.getKey(), entry.getValue());
             }
         }
-
+        // 覆盖
         Set<String> newUnauthorizedTopics = fillSet(addUnauthorizedTopics, unauthorizedTopics, shouldRetainTopic);
         Set<String> newInvalidTopics = fillSet(addInvalidTopics, invalidTopics, shouldRetainTopic);
         Set<String> newInternalTopics = fillSet(addInternalTopics, internalTopics, shouldRetainTopic);
-
+        // 构造新的元数据缓存
         return new MetadataCache(newClusterId, newNodes, newMetadataByPartition.values(), newUnauthorizedTopics,
                 newInvalidTopics, newInternalTopics, newController);
     }
 
     /**
+     * 复制{@code baseSet}并在{@code fillSet}中添加所有不存在的元素，使{@code predicate}为真。
+     * 换句话说，{@code baseSet}的所有元素都将包含在结果中，在predicate为true的情况下，{@code fillSet}中将包含额外的非重叠元素。
+     *
      * Copies {@code baseSet} and adds all non-existent elements in {@code fillSet} such that {@code predicate} is true.
      * In other words, all elements of {@code baseSet} will be contained in the result, with additional non-overlapping
      * elements in {@code fillSet} where the predicate is true.
@@ -180,6 +202,9 @@ public class MetadataCache {
                 invalidTopics, internalTopics, controller);
     }
 
+    /**
+     * 根据address构建节点元数据缓存
+     */
     static MetadataCache bootstrap(List<InetSocketAddress> addresses) {
         Map<Integer, Node> nodes = new HashMap<>();
         int nodeId = -1;
@@ -192,6 +217,10 @@ public class MetadataCache {
                 null, Cluster.bootstrap(addresses));
     }
 
+    /**
+     * 创建空的节点元数据将缓存
+     * @return
+     */
     static MetadataCache empty() {
         return new MetadataCache(null, Collections.emptyMap(), Collections.emptyList(),
                 Collections.emptySet(), Collections.emptySet(), Collections.emptySet(), null, Cluster.empty());
