6b1cc902f7cbdb1410ea95e8640a8a4f38d6baf2
lee9210 <597969610@qq.com>
2020-10-19 23:30:37 +0800

    ProducerMetadata 注释

--- clients/src/main/java/org/apache/kafka/clients/producer/internals/ProducerMetadata.java
+++ clients/src/main/java/org/apache/kafka/clients/producer/internals/ProducerMetadata.java
@@ -32,12 +32,18 @@ import java.util.Map;
 import java.util.Objects;
 import java.util.Set;
 
+/**
+ * producer元数据
+ */
 public class ProducerMetadata extends Metadata {
     // If a topic hasn't been accessed for this many milliseconds, it is removed from the cache.
+    /** 如果某个主题在这么多毫秒内没有被访问，那么它将从缓存中删除。 */
     private final long metadataIdleMs;
 
     /* Topics with expiry time */
+    /** topic过期时间 */
     private final Map<String, Long> topics = new HashMap<>();
+    /** 新创建topic */
     private final Set<String> newTopics = new HashSet<>();
     private final Logger log;
     private final Time time;
@@ -54,25 +60,39 @@ public class ProducerMetadata extends Metadata {
         this.time = time;
     }
 
+    /**
+     * 构造并返回元数据请求生成器，用于获取集群数据和所有活动topic。
+     */
     @Override
     public synchronized MetadataRequest.Builder newMetadataRequestBuilder() {
         return new MetadataRequest.Builder(new ArrayList<>(topics.keySet()), true);
     }
 
+    /**
+     * 构造并返回一个元数据请求生成器，用于获取集群数据和任何未激活的主题
+     */
     @Override
     public synchronized MetadataRequest.Builder newMetadataRequestBuilderForNewTopics() {
         return new MetadataRequest.Builder(new ArrayList<>(newTopics), true);
     }
 
+    /**
+     * 新增一个topic，并设置需要更新元数据
+     */
     public synchronized void add(String topic, long nowMs) {
         Objects.requireNonNull(topic, "topic cannot be null");
         if (topics.put(topic, nowMs + metadataIdleMs) == null) {
             newTopics.add(topic);
+            // 设置需要更新
             requestUpdateForNewTopics();
         }
     }
 
+    /**
+     * 设置topic需要更新
+     */
     public synchronized int requestUpdateForTopic(String topic) {
+        // 如果是新增主题
         if (newTopics.contains(topic)) {
             return requestUpdateForNewTopics();
         } else {
@@ -90,10 +110,20 @@ public class ProducerMetadata extends Metadata {
         return newTopics;
     }
 
+    /**
+     * 检查是否包含topic
+     */
     public synchronized boolean containsTopic(String topic) {
         return topics.containsKey(topic);
     }
 
+    /**
+     * 检查是否包含topic，或者topic是否删除
+     * @param topic
+     * @param isInternal
+     * @param nowMs
+     * @return
+     */
     @Override
     public synchronized boolean retainTopic(String topic, boolean isInternal, long nowMs) {
         Long expireMs = topics.get(topic);
@@ -111,6 +141,8 @@ public class ProducerMetadata extends Metadata {
     }
 
     /**
+     * 等待元数据更新，直到当前版本大于我们知道的上一个版本
+     *
      * Wait for metadata update until the current version is larger than the last version we know of
      */
     public synchronized void awaitUpdate(final int lastVersion, final long timeoutMs) throws InterruptedException {
@@ -122,32 +154,52 @@ public class ProducerMetadata extends Metadata {
             return updateVersion() > lastVersion || isClosed();
         }, deadlineMs);
 
-        if (isClosed())
+        if (isClosed()) {
             throw new KafkaException("Requested metadata update after close");
+        }
     }
 
+    /**
+     * 更新集群元数据
+     * 如果启用了topic过期，则为必要的topic设置过期时间，过期的topic将从元数据中删除。
+     *
+     * @param requestVersion The request version corresponding to the update response, as provided by
+     *     {@link #newMetadataRequestAndVersion(long)}.
+     * @param response metadata response received from the broker
+     * @param isPartialUpdate whether the metadata request was for a subset of the active topics
+     * @param nowMs current time in milliseconds
+     */
     @Override
     public synchronized void update(int requestVersion, MetadataResponse response, boolean isPartialUpdate, long nowMs) {
+        // 更新集群元数据
         super.update(requestVersion, response, isPartialUpdate, nowMs);
 
         // Remove all topics in the response that are in the new topic set. Note that if an error was encountered for a
         // new topic's metadata, then any work to resolve the error will include the topic in a full metadata update.
+        // 删除newTopics中已经更新的信息
         if (!newTopics.isEmpty()) {
             for (MetadataResponse.TopicMetadata metadata : response.topicMetadata()) {
                 newTopics.remove(metadata.topic());
             }
         }
-
-        notifyAll();
-    }
-
-    @Override
-    public synchronized void fatalError(KafkaException fatalException) {
-        super.fatalError(fatalException);
+        // 唤醒所有等待线程
         notifyAll();
     }
 
     /**
+     * 设置异常，并唤醒等待线程
+     * @param fatalException
+     */
+    @Override
+    public synchronized void fatalError(KafkaException fatalException) {
+        // 设置异常
+        super.fatalError(fatalException);
+        // 唤醒所有等待线程
+        notifyAll();
+    }
+
+    /**
+     * 关闭实例，表示不再可能进行元数据更新。并唤醒所有线程
      * Close this instance and notify any awaiting threads.
      */
     @Override
